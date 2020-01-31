7b04bb53feaa48b1973b9890686d75d95f2b0427
Max Schäfer <max-schaefer-lg@gmx.de>
2020-01-31 23:34:04 +0100

    Add Sentry

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,10 @@
+import * as Sentry from "@sentry/node";
+
+Sentry.init({
+    dsn: process.env.SENTRY_DSN,
+    environment: process.env.ENV
+});
+
 import { CahServer } from "./lib/CahServer";
 
 let cahServer = new CahServer();
