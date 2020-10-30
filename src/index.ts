20f6fc8341ce2b575020733eb811ea418e9995d2
Max Schäfer <max-schaefer-lg@gmx.de>
2020-10-30 14:28:36 +0100

    Set sentry as optional

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,13 @@
 import * as Sentry from "@sentry/node";
 
-Sentry.init({
-    dsn: process.env.SENTRY_DSN,
-    environment: process.env.ENV
-});
+if (process.env.ENABLE_SENTRY) {
+    if (!process.env.SENTRY_DSN)
+        throw new Error('If you want to use sentry, please set SENTRY_DSN!');
+    Sentry.init({
+        dsn: process.env.SENTRY_DSN,
+        environment: process.env.ENV
+    });
+}
 
 import { CahServer } from "./lib/CahServer";
 
