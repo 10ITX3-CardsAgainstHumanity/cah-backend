b27509ce0fdf34dc2e845f7845189103f4500bd6
Max Schäfer <max-schaefer-lg@gmx.de>
2020-10-31 13:32:49 +0100

    Reindent files with tabs

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,12 @@
 import * as Sentry from "@sentry/node";
 
 if (process.env.ENABLE_SENTRY) {
-    if (!process.env.SENTRY_DSN)
-        throw new Error('If you want to use sentry, please set SENTRY_DSN!');
-    Sentry.init({
-        dsn: process.env.SENTRY_DSN,
-        environment: process.env.ENV
-    });
+	if (!process.env.SENTRY_DSN)
+		throw new Error('If you want to use sentry, please set SENTRY_DSN!');
+	Sentry.init({
+		dsn: process.env.SENTRY_DSN,
+		environment: process.env.ENV
+	});
 }
 
 import { CahServer } from "./lib/CahServer";
--- src/types.ts
+++ src/types.ts
@@ -3,37 +3,37 @@ import {BlackCard} from "./lib/BlackCard";
 import {Player} from "./lib/Player";
 
 export enum GameState {
-    'unstarted',
-    'start',
-    'selection',
-    'judging'
+	'unstarted',
+	'start',
+	'selection',
+	'judging'
 }
 
 export interface Card {
-    readonly text: string;
-    getText(): string;
+	readonly text: string;
+	getText(): string;
 }
 
 export interface PlayerResponse {
-    id: String,
-    username: String,
-    isCzar?: boolean
+	id: String,
+	username: String,
+	isCzar?: boolean
 }
 
 export interface allChoosedPlayerCardsResponse {
-    [index: number]: {
-        playerId: string,
-        [cards: number]: Partial<WhiteCard>
-    }
+	[index: number]: {
+		playerId: string,
+		[cards: number]: Partial<WhiteCard>
+	}
 }
 
 export interface ResponseMessage {
-    status: boolean,
-    msg?: {
-        state?: GameState
-        player?: PlayerResponse,
-        players?: Partial<Player>[]
-        card?: Partial<BlackCard>,
-        cards?: Partial<WhiteCard>[]
-    } | string
+	status: boolean,
+	msg?: {
+		state?: GameState
+		player?: PlayerResponse,
+		players?: Partial<Player>[]
+		card?: Partial<BlackCard>,
+		cards?: Partial<WhiteCard>[]
+	} | string
 }
